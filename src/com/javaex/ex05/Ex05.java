0d14b3a1f6efda31bcd3270823665be3eef41039
kimsy <duddl2102@naver.com>
2020-11-20 13:05:22 +0900

    배열 수업내용

---
+++ src/com/javaex/ex05/Ex05.java
@@ -0,0 +1,17 @@
+package com.javaex.ex05;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 3;
+		
+		int num2 = num;
+		
+		System.out.println("1. " + num + " " + num2);
+		
+		num2 = 7;
+		System.out.println("2. " + num + " " + num2);
+	}
+
+}
